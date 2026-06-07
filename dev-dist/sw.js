60335e4dc8084be36fa22e244c195d876bf44b28
TarunShakya <shakyatarun32@gmail.com>
2026-06-08 00:47:44 +0530

    Ui Changed7o0988

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-290dd570'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uu2li5nunjg"
+    "revision": "0.i9qlm4jtgrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
